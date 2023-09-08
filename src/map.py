d801ef733fcdb23d7f96d1fba88f775e455f7fef
Davoud Taghawi-Nejad <davoud.taghawi-nejad@whiteshieldpartners.com>
2023-09-08 15:12:56 +0400

    Displays and filters map data

--- src/map.py
+++ src/map.py
@@ -4,6 +4,7 @@ from plotly import graph_objects as go
 from geopy.geocoders import Nominatim
 from tqdm import tqdm as progress_bar
 from perscache import Cache
+from numpy import percentile
 
 
 ROWS, COLUMNS = 0, 1
@@ -37,7 +38,8 @@ def get_unique_countries(series):
 def plot_network_on_world_map(df):
     fig = go.Figure()
 
-    unique_countries = get_unique_countries(df['reporterISO'])
+    unique_countries = set(get_unique_countries(
+        df['reporterISO'])) + set(get_unique_countries(df['partnerISO']))
     unique_countries = unique_countries.set_index('ISO')
     fig.add_trace(go.Scattergeo(
                   # locationmode = 'USA-states',
@@ -55,8 +57,8 @@ def plot_network_on_world_map(df):
                           color='rgba(68, 68, 68, 0)'))))
 
     for i in progress_bar(range(len(df))):
-        reporter_lat, reporter_long = unique_countries.loc[df['reporterISO'][i]]
-        partner_lat, partner_long = unique_countries.loc[df['partnerISO'][i]]
+        reporter_lat, reporter_long = unique_countries.loc[df['reporterISO'].iloc[i]]
+        partner_lat, partner_long = unique_countries.loc[df['partnerISO'].iloc[i]]
 
         fig.add_trace(
             go.Scattergeo(
@@ -65,7 +67,8 @@ def plot_network_on_world_map(df):
                 lat=[reporter_lat, partner_lat],
                 mode='lines',
                 line=dict(width=1, color='red'),
-                opacity=float(df['fobvalue'][i]) / float(df['fobvalue'].max()),
+                opacity=float(df['fobvalue'].iloc[i]) / \
+                float(df['fobvalue'].max()),
             )
         )
 
@@ -85,3 +88,17 @@ def plot_network_on_world_map(df):
         hovertemplate="%{customdata}"))
 
     fig.show()
+
+
+def filter_quantiles(df, quantile):
+    selected = []
+    for partner in df['partnerISO'].unique():
+        all_trade_links_of_partner = df[df['partnerISO'] == partner]
+        cut_off = percentile(
+            all_trade_links_of_partner['fobvalue'], 100 - quantile)
+        selected.append(
+            all_trade_links_of_partner[all_trade_links_of_partner['fobvalue'] > cut_off])
+
+    selected = pd.concat(selected, axis=ROWS)
+
+    return selected
